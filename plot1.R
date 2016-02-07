e991740ab3c878fd1d50a17ab805b9cf04935f7c
pchoengtawee <pin.cullen@gmail.com>
2016-02-07 14:15:34 -0500

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,4 +1,4 @@
-rawdata <- "/Users/Pinny/Documents/DataSience_JHK/Data Sciencist's Toolbox/Exploratory Data Analysis/Week1 Assignment/household_power_consumption.txt"
+rawdata <- "./Exploratory Data Analysis/Week1 Assignment/household_power_consumption.txt"
 data <- read.table(rawdata, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
 subData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
 
